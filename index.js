250f0e0357731a5f9d99fefe6ea732a1497959cd
Elise McClain <celisemcclain@gmail.com>
2023-06-02 19:40:21 -0500

    looping

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"]
+let event = ["surprise"]
+
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+  
+  
+  function countDown(n) {
+    let i = 10
+        while (i >= 0) {
+       console.log(i)
+       i--
+  }
+return n
+}
+countDown(n)
